fd260a704d94522e23f6f130f621adc30750da46
Aqau3k <c3ryumon@gmail.com>
2025-06-20 17:59:01 +0900

    コマンドライン引数からパスを取得する処理を追加する

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+
+/// コマンドライン引数からファイルパスを返す
+fn get_file_path() -> Result<String, io::Error> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("ファイルパスを引数として指定してください。");
+        return Err(io::Error::new(io::ErrorKind::InvalidInput, "ファイルパスが不足しています"));
+    }
+    Ok(args[1].clone())
+}
 
 fn open_file_as_reader(path: &str) -> io::Result<BufReader<File>> {
     let file = File::open(path)?;
@@ -7,8 +18,8 @@ fn open_file_as_reader(path: &str) -> io::Result<BufReader<File>> {
 }
 
 fn main() -> io::Result<()> {
-    let path = "input.txt";
-    let reader = open_file_as_reader(path)?;
+    let path = get_file_path()?;
+    let reader = open_file_as_reader(&path)?;
 
     for line_result in reader.lines() {
         let line = line_result?;

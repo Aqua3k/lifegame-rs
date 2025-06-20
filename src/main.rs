d61941397ac3443dd0c00e09385c3c0fb33c5d13
Aqau3k <c3ryumon@gmail.com>
2025-06-20 17:46:50 +0900

    ファイル入力して出力する処理を書いた

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Welcome to LifeGame in Rust!");
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn open_file_as_reader(path: &str) -> io::Result<BufReader<File>> {
+    let file = File::open(path)?;
+    Ok(BufReader::new(file))
+}
+
+fn main() -> io::Result<()> {
+    let path = "input.txt";
+    let reader = open_file_as_reader(path)?;
+
+    for line_result in reader.lines() {
+        let line = line_result?;
+        println!("{}", line);
+    }
+
+    Ok(())
 }
